ffd06d91bd13a508ee332f84defd565946b3b0d7
bk <kblessingtatenda@gmail.com>
2024-07-26 23:28:33 +0200

    updated

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes/routeChanges.js';
+import cors from 'cors';
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', routes);
 export default app;
